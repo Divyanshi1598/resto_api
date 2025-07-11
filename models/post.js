af9e4b14c2a458361d69b90a3b518f5974e7bd8a
Divyanshi1598 <15tiwaridivyanshi@gmail.com>
2025-07-11 16:02:51 +0530

    change

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   title: String,
   content: String,
-  // Add other fields as needed
+ 
 });
 
 module.exports = mongoose.model('Post', postSchema); 
